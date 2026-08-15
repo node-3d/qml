66b49784222be60cb9d97ea5bf79859063e3de89
Luis Blanco <luisblanco1337@gmail.com>
2026-08-15 23:25:36 +0400

    Keep QML release at 5.0.0

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 import { install } from '@node-3d/addon-tools';
 
 const prefix = 'https://github.com/node-3d/qml/releases/download';
-const tag = '5.0.1';
+const tag = '5.0.0';
 
 await install(`${prefix}/${tag}`);
